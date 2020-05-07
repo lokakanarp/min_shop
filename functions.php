a9f8465b6ec6bf0e1eb935ce9cee7c6d0896a998
Loka Kanarp <lokakanarp@gmail.com>
2020-05-07 21:30:26 +0200

    Fixed bug with swedish weekdays.

--- functions.php
+++ functions.php
@@ -1,15 +1,26 @@
 <?php
 
-function even_day_odd_week() { //Avgör om det är en jämn dag, udda vecka, mellan kl 13-17.
+function even_day_odd_week() { //determine whether it is a even date, odd week, between 11-17.
    if(date("W") % 2 != 0 && date("j") % 2 == 0 && date("H") >= 11 && date("H") <= 17){
      return true;
    }
 }
-function swedish_date(){ //Ger dag, datum, månad, vecka och klockslag.
-    //setlocale(LC_ALL,"sv_SE");  //Ger svenska veckodagar och månader.
-     echo "<p class='datum'>" . strftime("%A %B %e") . "</p>
-          <p class='datum'>Week " . date("W") . "</p>
-          <p class='datum'>Time: " . date("H") . ":" . date("i") . "</p>";
+function swedish_date() {
+  setlocale(LC_ALL,"sv_SE");  //Month in Swedish
+  $day = date("l");
+
+  $weekdays = array("Monday" => "Måndagen",
+  "Tuesday" => "Tisdagen",
+  "Wednesday" => "Onsdagen",
+  "Thursday" => "Torsdagen",
+  "Friday" => "Fredagen",
+  "Saturday" => "Lördagen",
+  "Sunday" => "Söndagen"
+  );
+  $swedishWeekday = $weekdays[$day];
+ echo "<p class='datum'>$swedishWeekday den" . strftime("%e %B") . "</p>
+      <p class='datum'>Vecka " . date("W") . "</p>
+      <p class='datum'>Klockan " . date("H") . ":" . date("i") . "</p>";
 }
 function rea_bild(){
     echo "<img src='images/image_rea.jpg'>";
